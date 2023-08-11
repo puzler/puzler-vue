0c6835dbac9930a768f394003301c1a40b9fe280
Charlie Pugh <charlie@jazzwealth.com>
2023-08-11 11:25:21 -0400

    add bugsnag

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import './assets/main.css'
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginVue from '@bugsnag/plugin-vue'
 
+Bugsnag.start({
+  apiKey: import.meta.env.VITE_PUZLER_BUGSNAG_API_KEY,
+  plugins: [new BugsnagPluginVue()]
+})
+
+import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { FaIcon } from '@/utils/font-awesome'
@@ -8,6 +15,9 @@ import router from '@/plugins/router'
 import App from '@/App.vue'
 
 const app = createApp(App)
+
+const bugsnagVue = Bugsnag.getPlugin('vue')
+if (!!bugsnagVue) app.use(bugsnagVue)
 
 app.use(createPinia())
 app.use(router)

b04f020956afbd65d9bc2e732c8a2177e4c75d3c
Charlie Pugh <charlie@jazzwealth.com>
2023-08-23 00:14:33 -0400

    make a empty grid come up on solve page with no puzzle data

--- src/views/SolverView.vue
+++ src/views/SolverView.vue
@@ -5,6 +5,7 @@ import PuzzleFetcher from '@/utils/puzzle-fetcher'
 import type { Puzzle } from '@/graphql/generated/types'
 import PuzzlePlayer from '@/components/player/PuzzlePlayer.vue'
 import { onMounted } from 'vue'
+import { PuzzleSolve } from '@/types'
 
 const fPuzzle = useRoute().query.fPuzzle
 const props = defineProps<{
@@ -18,6 +19,9 @@ onMounted(async () => {
     puzzle.value = await PuzzleFetcher.fetchById(props.puzzleId)
   } else if (fPuzzle) {
     puzzle.value = await PuzzleFetcher.fetchFPuzzle(fPuzzle as string)
+  } else {
+    const temp = new PuzzleSolve({ size: 9 })
+    puzzle.value = temp.puzzleData
   }
 })
 
